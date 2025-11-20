2e7ec71a861f278a6c3aa5a40b884eed20cba00c
Alhousainy Abdelrahman <125814743+Alhousainy23@users.noreply.github.com>
2025-11-20 13:00:45 +0200

    Update 5. Bit++.py

--- 5. Bit++.py
+++ 5. Bit++.py
@@ -1,8 +1,10 @@
 #Bit Problem 
+# Link Problem ==> https://codeforces.com/contest/282/problem/A
 num_of_operations = int(input())
 x = 0
 for i in range (num_of_operations):
     instructions = input()
     if '++' in instructions: x+=1
     else : x-=1
+
 print(x)
